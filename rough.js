22fa186e1a478e2a396422d33f778beac6767871
RIYANSHI <riyanshig.cs.19@nitj.ac.in>
2022-03-17 20:35:56 +0530

    rough page add

--- rough.js
+++ rough.js
@@ -1,2 +1,39 @@
 // all api from backend
 
+
+// pending work
+
+
+Backend 
+
+// Login correct
+// AUCTION PRODUCT DETAIL FROM BACKEND
+// SUGGESTION ALGO
+//FILTERING AUCTIONS ON LOCATION AND DATE
+
+
+// BIDDING PAGE CONNECT FROM BACKEND
+
+//Can be done
+
+// TIME SUGGESTION
+// MAIL PART nodemailer
+// WINNER CHAT OPTION 
+
+
+// BIDDING COMMENTS
+// NEWS FEED
+// ANY OTHER ALGO SHOULD BE THERE
+
+
+UI
+// WINNER CHAT OPTION - ui
+// FEED UI
+// BIDDING PAGE
+// registered auctions/ organized auction - ui
+
+
+
+
+
+

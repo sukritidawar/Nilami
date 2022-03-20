563e43b07da13e614643873768f77d1f921f7390
shobha k <shobha9250@gmail.com>
2022-03-21 01:48:30 +0530

    promise tracker changes

--- src/auction/CreatedAuction.js
+++ src/auction/CreatedAuction.js
@@ -88,7 +88,7 @@ const CreatedAuction = () => {
                   onHide={handleCloseInfoModal} 
                 /> */}
               </>
-              // )} 
+               {/* )}  */}
           </>
         {/* )} */}
       </>

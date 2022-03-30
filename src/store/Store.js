13bafe076b05ad6f4d2aea9ac1024da9a4aa109a
sukriti <sukritidawar@gmail.com>
2022-03-30 21:31:22 +0530

    Code formatting and UI fixes

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,9 +1,9 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 
 const Store = createContext({
-    isAuth: false,
-    user_id: null,
-    user_name: null,
-})
+  isAuth: false,
+  user_id: null,
+  user_name: null,
+});
 
-export default Store
+export default Store;
